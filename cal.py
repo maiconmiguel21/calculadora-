41fa64c6eab0c66e5a4384cf046ca40f79534d6e
maiconmiguel21 <andersinhotemiguel2203@gmail.com>
2026-03-28 10:27:38 -0300

    teste

---
+++ cal.py
@@ -0,0 +1,18 @@
+n1 = float(input('Digite um número: '))
+n2 = float(input('Digite outro número: '))
+operacao = input('Digite a operação desejada (+, -, *, /): ')
+
+if operacao == '+':
+   resultado = n1 + n2
+
+elif operacao == '-':
+    resultado = n1-n2
+
+elif operacao == '*':
+    resultado = n1*n2
+
+elif operacao == '/':
+    resultado = n1/n2
+
+
+
